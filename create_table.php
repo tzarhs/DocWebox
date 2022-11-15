95b45d370ad9c79162d28177076f97378592e94a
LAPTOP-H8IIVLSB\giann <ics20090@uom.edu.gr>
2022-11-15 13:38:57 +0200

    test_create_table

--- create_table.php
+++ create_table.php
@@ -1,3 +1,49 @@
 <?php
     include("connect.php");
+
+    // Έλεγχος αν υπάρχει ο πίνακας appointment
+    if(mysqli_query($link,"DESCRIBE  appointment ")){
+        // Αν υπάρχει μην κάνεις τίποτε
+    }else{
+        // Αν δεν υπάρχει δημιούργησέ τον
+        echo "Ο πίνακας appointment δεν υπάρχει... ";
+        // Δημιουργία πίνακα food
+        $sql = "CREATE TABLE IF NOT EXISTS appointment (
+        id INT(11) AUTO_INCREMENT PRIMARY KEY,
+        date DATETIME,
+        doctor_id INT(11),
+        patient_id INT(11),
+        )";
+        if ($link->query($sql) === TRUE) {
+            echo "ΔΗΜΙΟΥΡΓΗΘΗΚΕ!<br/>";
+            
+        } else {
+            echo "Πρόβλημα στη δημιουργία του πίνακα appointment: " . $link->error . "<br/>";
+        }
+    } 
+
+    // Έλεγχος αν υπάρχει ο πίνακας doctor
+    if(mysqli_query($link,"DESCRIBE  doctor ")){
+        // Αν υπάρχει μην κάνεις τίποτε
+    }else{
+        // Αν δεν υπάρχει δημιούργησέ τον
+        echo "Ο πίνακας doctor δεν υπάρχει... ";
+        // Δημιουργία πίνακα doctor
+        $sql = "CREATE TABLE IF NOT EXISTS doctor (
+        id INT(11) AUTO_INCREMENT PRIMARY KEY,
+        doctor_name VARCHAR(250) NOT NULL,
+        location VARCHAR(250) NOT NULL,
+        profession_id INT(11) NOT NULL,
+        appointment_id INT(11) NOT NULL,
+        )";
+        if ($link->query($sql) === TRUE) {
+            echo "ΔΗΜΙΟΥΡΓΗΘΗΚΕ!<br/>";
+            
+        } else {
+            echo "Πρόβλημα στη δημιουργία του πίνακα doctor: " . $link->error . "<br/>";
+        }
+    } 
+
+    mysqli_close($link);
+
 ?>
