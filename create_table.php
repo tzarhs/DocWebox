e381c46b54ab07ae989c2147f569c9a140da644a
DESKTOP-SMRD5CI\jtzar <j.tzaris@gmail.com>
2022-11-15 13:49:10 +0200

    Creating usertype and login tables.

--- create_table.php
+++ create_table.php
@@ -1,3 +1,35 @@
 <?php
     include("connect.php");
+
+    if(mysqli_query($link,"DESCRIBE usertype")){
+        //Ο πίνακας υπάρχει ήδη
+	}
+    else {
+        $sql="CREATE TABLE IF NOT EXISTS usertype(id INT(100) AUTO_INCREMENT PRIMARY KEY,user_type VARCHAR(60) NOT NULL	)";
+        if($link->query($sql)===TRUE){
+            echo "Δημιουργήθηκε ο πίνακας usertype!<br/>";
+            mysqli_query($link,"INSERT INTO usertype VALUES(NULL, 'admin')");
+            mysqli_query($link,"INSERT INTO usertype VALUES(NULL, 'patient')");
+            mysqli_query($link,"INSERT INTO usertype VALUES(NULL, 'doctor')");
+            
+        }
+        else{
+			echo "Πρόβλημα στη δημιουργία του πίνακα data: ".$link->error."<br/>";
+		}
+    }
+    
+
+    if(mysqli_query($link,"DESCRIBE login")){
+        //Ο πίνακας υπάρχει ήδη
+	}
+    else {
+        $sql="CREATE TABLE IF NOT EXISTS login(id INT(100) AUTO_INCREMENT PRIMARY KEY, username VARCHAR(60) NOT NULL, password VARCHAR(60) NOT NULL, user_type VARCHAR(60) NOT NULL";
+        if($link->query($sql)===TRUE){
+        echo("Δημιουργήθηκε ο πίνακας login!");
+        }
+        else{
+			echo "Πρόβλημα στη δημιουργία του πίνακα login: ".$link->error."<br/>";
+		}
+    }
+    mysqli_close($link);
 ?>
