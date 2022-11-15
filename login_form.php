e194f4348cbcd0f4ff56498afad5ca1f3ea6f90f
DESKTOP-SMRD5CI\jtzar <j.tzaris@gmail.com>
2022-11-15 23:23:17 +0200

    Same files , but with adjustments.

--- login_form.php
+++ login_form.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="styles.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <link rel="stylesheet" href="css/styles.css">
+
     <h1>Σύνδεση/Εγγραφή</h1>
-    <form action="">
-        <label for="fname">First name:</label>
-        <input type="text" id="fname" name="fname"><br><br>
-        <label for="fname">Last name:</label>
-        <input type="text" id="lname" name="lname"><br><br>
+    <form>
+        <label for="fname">First name:
+        <input type="text" id="fname" name="fname"></label><br><br>
+        <label for="fname">Last name:
+        <input type="text" id="lname" name="lname"></label><br><br>
         <button type="button">ΣΥΝΔΕΣΗ</button>
     </form>
         
