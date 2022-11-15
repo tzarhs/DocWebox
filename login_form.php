2380f5f0654dcc258624af8f90e9d0c1fe890b2a
DESKTOP-SMRD5CI\jtzar <j.tzaris@gmail.com>
2022-11-15 15:08:33 +0200

    Login form and css file.

---
+++ login_form.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <link rel="stylesheet" href="css/styles.css">
+    <h1>Σύνδεση/Εγγραφή</h1>
+    <form action="">
+        <label for="fname">First name:</label>
+        <input type="text" id="fname" name="fname"><br><br>
+        <label for="fname">Last name:</label>
+        <input type="text" id="lname" name="lname"><br><br>
+        <input type="submit" value="Submit">
+</form>
+        
+    
+</body>
+</html>
+
+<?php
+    include("connect.php");
+?>
+
