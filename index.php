99e9278c276eff1e4bcebafe1455d2de82f4a3b6
Dimitris Chios <chiosdimit@gmail.com>
2022-11-20 13:01:13 +0200

    Created the footer

--- index.php
+++ index.php
@@ -4,13 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="container">
+        <h1 class="motto">Health is Wealth!</h1></div>
+
+        <div class="footer">
+            <div class="footer-heading footer-1">
+                <a href="#"><h3>DocWebox &copy;2022</h3></a></div>
+            <div class="footer-heading footer-2">
+                <h3>Σχετικά με εμάς</h3>
+                <a href="#">Η Εταιρία μας</a>
+                <a href="#">Η Ομάδα μας</a></div>
+            <div class="footer-heading footer-3">
+                <h3><a href="#">Πολιτική Απορρήτου</a></h3></div>
+            <div class="footer-heading footer-3">
+                <h3><a href="#">Όροι Χρήσης</a></h3></div>
+            </div>
 </body>
 </html>
 
 <?php
     include("connect.php");
 ?>
+   
