95b6ef266b05373911100db4eec3836dde3b3e4e
DESKTOP-SMRD5CI\jtzar <j.tzaris@gmail.com>
2022-11-29 12:35:11 +0200

    Idk

--- index.php
+++ index.php
@@ -1,13 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>DocWebox-Οι καλύτεροι γιατροί στην διάθεση σου!</title>
 </head>
 <body>
-    
+
+<nav>
+      <input type="checkbox" id="check">
+      <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+      </label>
+      <label class="logo">Αρχική Σελίδα</label>
+      <ul>
+        <li><a class="active" href="#">Ειδικότητες</a></li>
+        <li><a href="#">Σύνδεση/Εγγραφή</a></li>
+        <li><a href="#">Σχετικά με εμάς</a></li>
+      </ul>
+</nav>
+
+<div class="container">
+    <form action="" class="search-bar">
+        <input type="text" placeholder="Ειδικότητα" name="profession">
+        <input type="text" placeholder="Περιοχή" name="location">
+        <button type="submit"><img src="search.png"></button>
+    </form>
+</div>
+
 </body>
 </html>
 
