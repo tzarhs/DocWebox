5b8a914b24047c2b93eff5885259b0d2292fb00e
DESKTOP-SMRD5CI\jtzar <j.tzaris@gmail.com>
2022-11-29 13:26:30 +0200

    idk

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="styles.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
