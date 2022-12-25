1af7f0aee2bf91516d70835d63ab038de985162f
Giannis Tzaris <j.tzaris@gmail.com>
2022-12-25 21:12:25 +0200

    no message

--- create_acc.php
+++ create_acc.php
@@ -24,11 +24,7 @@
       </ul>
 </nav>
 
-<<<<<<< HEAD
 <form action="signup.php" class="box" method ="post">
-=======
-<form action="signup.php" class="box" mehtod ="post">
->>>>>>> c68ffc4a4044ca27be997df361b6d388a9915565
 
         <h1>Sign up</h1>
         <br> 
