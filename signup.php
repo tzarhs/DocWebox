24edef3516bb53fa1cd9569d851d8f6b52cd701e
LAPTOP-RJSSE7G5\johns <johnstamkopoulos@gmail.com>
2022-12-27 21:16:27 +0200

    alert sign up

--- signup.php
+++ signup.php
@@ -1,3 +1,13 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+</head>
+<body>
 <?php
 
 session_start();
@@ -13,25 +23,26 @@ if (isset( $_POST['submit'])) {
 
      
         if($_POST['select'] == "option1"){
-                $result = mysqli_query($link,"SELECT * FROM patient WHERE name='$username'");
-                $num_rows = mysqli_num_rows($result);
-                if ($num_rows) {
-                        header("location: create_acc.php?signup=failed"); 
+                $query1 = "SELECT * FROM patient WHERE name='$username'";
+                $result1 = mysqli_query($link, $query1);
+
+                if (mysqli_num_rows($result1) > 0) {
                         $error = "Signup Failed! Username already exists";
-                        echo "<script type='text'/javascript'>alert('$error');</script>";
-                        
+                        echo "<script type='text/javascript'>alert('$error');</script>";
+                        header('location: login.php?login=failed');
                 }else{
                         mysqli_query($link,"INSERT INTO patient(name, password)
                         VALUES ('$username', '$password')");
                         header("Location: index.php?signup=success");    
                 }
         }elseif($_POST['select'] == "option2"){
-                $result = mysqli_query($link,"SELECT * FROM doctor WHERE doctor_name='$username'");
-                $num_rows = mysqli_num_rows($result);
-                if ($num_rows) {
-                        header("location: create_acc.php?signup=failed"); 
+                $query2 = "SELECT * FROM doctor WHERE doctor_name='$username'";
+                $result2 = mysqli_query($link, $query2);
+
+                if (mysqli_num_rows($result2) > 0) {
                         $error = "Signup Failed! Username already exists";
-                        echo "<script type='text'/javascript'>alert('$error');</script>";
+                        echo "<script type='text/javascript'>alert('$error');</script>";
+                        header('location: login.php?login=failed');
                 }else{
                         mysqli_query($link,"INSERT INTO doctor(profession_id, location, doctor_name, password)
                         VALUES ('$profession','$location', '$username', '$password')");
@@ -42,3 +53,5 @@ if (isset( $_POST['submit'])) {
         
 }
 ?>
+</body>
+</html>
