7d0444be673492d6833885ff49c1b6aba8202f7c
DESKTOP-SMRD5CI\jtzar <j.tzaris@gmail.com>
2023-01-10 11:21:55 +0200

    nthg

--- index.php
+++ index.php
@@ -26,28 +26,6 @@
     </nav>
 
     <div class="container">
-<<<<<<< HEAD
-      <h1>Health is wealth</h1>
-    <form method="POST" action="" class="search-bar">
-        <input type="text" placeholder="Ειδικότητα" name="profession">
-        <input type="text" placeholder="Περιοχή" name="location">
-        <button type="submit"><img src="search.png"></button>
-    </form>
-    </div>
-    <?php
-      if(isset($_POST["submit"])){
-        $prof = $_POST["profession"];
-        $loc = $_POST["location"];
-
-        $result = 	mysqli_query($link, "SELECT * FROM profession WHERE name='$prof' ");
-        while ($row = mysqli_fetch_array($result)) {
-          echo $row[1];
-        }
-        mysqli_close($link); 
-      }
-    ?>    
-    <div class="footer">
-=======
       <!-- <h1>Αναζητήστε τον γιατρό που θέλετε!</h1> -->
       <form action="search.php" class="search-bar" method="POST">
         <input type="text" placeholder="Ειδικότητα" name="profession">
@@ -57,7 +35,6 @@
     </div>
     
         <div class="footer">
->>>>>>> 6700eba349dd4e06fea8987a095285157d7e1c75
             <div class="footer-heading footer-1">
                 <a href="#"><h3>DocWebox &copy;2022</h3></a></div>
             <div class="footer-heading footer-2">
